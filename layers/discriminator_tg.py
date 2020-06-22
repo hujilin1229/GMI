46bdc5c79941719f2b6fc8b25b6fb8a7f8b65bcb
Jilin Hu <hujilin1229@163.com>
2020-06-22 16:55:20 +0400

    add tg operation

--- layers/discriminator_tg.py
+++ layers/discriminator_tg.py
@@ -1,9 +1,9 @@
 import torch
 import torch.nn as nn
 
-class Discriminator(nn.Module):
+class Discriminator_tg(nn.Module):
     def __init__(self, n_h1, n_h2):
-        super(Discriminator, self).__init__()
+        super(Discriminator_tg, self).__init__()
         self.f_k = nn.Bilinear(n_h1, n_h2, 1)
         self.act = nn.Sigmoid()
 
@@ -16,15 +16,26 @@ class Discriminator(nn.Module):
             if m.bias is not None:
                 m.bias.data.fill_(0.0)
 
-    def forward(self, h_c, h_pl, sample_list, s_bias1=None, s_bias2=None):
+    def forward(self, h_c, h_pl, sample_tensor, s_bias1=None, s_bias2=None):
+        """
+        calculate discriminator results
+
+        :param h_c: num_nodes x n_h2
+        :param h_pl: num_nodes x n_h1
+        :param sample_tensor: negative sampling
+        :param s_bias1: bias1
+        :param s_bias2: bias2
+        :return: num_nodes, num_nodes x num_neg
+        """
+
         sc_1 = torch.squeeze(self.f_k(h_pl, h_c), 2)
         sc_1 = self.act(sc_1)
-        sc_2_list = []
-        for i in range(len(sample_list)):
-            h_mi = torch.unsqueeze(h_pl[0][sample_list[i]],0)
-            sc_2_iter = torch.squeeze(self.f_k(h_mi, h_c), 2)
-            sc_2_list.append(sc_2_iter)
-        sc_2_stack = torch.squeeze(torch.stack(sc_2_list,1),0)
+
+        # h_pl, h_c: num_nodes x num_feature
+        # num_nodes x num_neg x num_feature
+        h_mi = h_pl[sample_tensor]
+        h_c_rep = torch.unsqueeze(h_c, dim=1).repeat([1, sample_tensor.size(1), 1])
+        sc_2_stack = torch.squeeze(self.f_k(h_mi, h_c_rep), 2)
         sc_2 = self.act(sc_2_stack)
 
         if s_bias1 is not None:
--- models/gmi_pooling.py
+++ models/gmi_pooling.py
@@ -1,26 +1,52 @@
 import torch
 import torch.nn as nn
 from utils import process
-from layers import GCN, AvgNeighbor, Discriminator
+from layers import GCN, AvgNeighbor, Discriminator, Discriminator_tg
+from torch_geometric.nn import GINConv
+from torch_geometric.nn.pool import sag_pool
+from torch_scatter import scatter_add, scatter_max
+
 
 class GMI(nn.Module):
     def __init__(self, n_in, n_h, activation):
         super(GMI, self).__init__()
         self.gcn1 = GCN(n_in, n_h, activation)  # if on citeseer and pubmed, the encoder is 1-layer GCN, you need to modify it
         self.gcn2 = GCN(n_h, n_h, activation)
-        self.disc1 = Discriminator(n_in, n_h)
-        self.disc2 = Discriminator(n_h, n_h)
+
+        self.act = nn.PReLU() if activation == 'prelu' else activation
+
+        self.gcn1_mlp = nn.Sequential(nn.Linear(n_in, n_h), self.act)
+        self.gcn1_1 = GINConv(self.gcn1_mlp, eps=0.0)
+
+        self.gcn2_mlp = nn.Sequential(nn.Linear(n_h, n_h), self.act)
+        self.gcn2_1 = GINConv(self.gcn2_mlp, eps=0.0)
+
+        self.disc1 = Discriminator_tg(n_in, n_h)
+        self.disc2 = Discriminator_tg(n_h, n_h)
         self.avg_neighbor = AvgNeighbor()
         self.prelu = nn.PReLU()
         self.sigm = nn.Sigmoid()
 
-    def forward(self, seq1, adj_ori, neg_num, adj, samp_bias1, samp_bias2):
+    def forward(self, data, neg_num, samp_bias1, samp_bias2):
+
+        x, edge_index, batch = data.x, data.edge_index, data.batch
         h_1, h_w = self.gcn1(seq1, adj)
         h_2, _ = self.gcn2(h_1, adj)
+
+        h_1_1 = self.gcn1_1(x, edge_index)
+        h_2_1 = self.gcn2_1(h_1_1, edge_index)
+
+        # I(h_i; x_i)
+        res_mi_pos, res_mi_neg = self.disc1(h_2_1, x, process.negative_sampling_tg(batch, neg_num), samp_bias1, samp_bias2)
+
+
         h_neighbor = self.prelu(self.avg_neighbor(h_w, adj_ori))
         """FMI (X_i consists of the node i itself and its neighbors)"""
         # I(h_i; x_i)
         res_mi_pos, res_mi_neg = self.disc1(h_2, seq1, process.negative_sampling(adj_ori, neg_num), samp_bias1, samp_bias2)
+
+
+
         # I(h_i; x_j) node j is a neighbor
         res_local_pos, res_local_neg = self.disc2(h_neighbor, h_2, process.negative_sampling(adj_ori, neg_num), samp_bias1, samp_bias2)
         """I(w_ij; a_ij)"""
